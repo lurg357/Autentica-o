dcbe8bb6827cf3bba9c01fdae67951e88573cf75
lurg357 <luizfelipedasilvasoares42@gmail.com>
2024-09-30 22:14:29 -0300

    Add files via upload

---
+++ 1/Untitled-2.ts
@@ -0,0 +1,15 @@
+// src/App.tsx
+
+import React from 'react';
+import Login from './components/Login';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Bem-vindo ao Meu App</h1>
+      <Login />
+    </div>
+  );
+};
+
+export default App;
